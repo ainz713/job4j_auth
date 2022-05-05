36b8716c25cf7880e89c563cb70fe7a2a9ba8d0c
ainz713 <ainz713@gmail.com>
2022-05-05 19:40:09 +0300

    0. RESTFull. Описание архитектуры

---
+++ README.md
@@ -0,0 +1,4 @@
+[![Build Status](https://app.travis-ci.com/ainz713/job4j_auth.svg?branch=main)](https://app.travis-ci.com/ainz713/job4j_auth)
+# job4j_auth
+
+Создание REST services
---
+++ src/main/java/ru/job4j/Job4jAuthApplication.java
@@ -0,0 +1,20 @@
+package ru.job4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Job4jAuthApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Job4jAuthApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Job4jAuthApplication.class, args);
+    }
+
+}
